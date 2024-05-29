42e4b20bac33949461d097e9868da71866ad7bd3
David Mghanga <damghanga@gmail.com>
2024-05-29 11:06:44 +0300

    Updated code formatting and removed reduntant function to pass linting.

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,14 +5,9 @@ Calculator functions
 
 def add(x, y):
     """Add x and y and return result."""
-    return x + y 
+    return x + y
 
 
 def subtract(x, y):
-    """Subract x from y and return result."""
-    return 0
-
-
-def subtract(x, y):
-    """subtract x from y and return result."""
-    return y - x 
+    """Subtract x from y and return result."""
+    return y - x
