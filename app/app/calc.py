429f031b2a2277d9ae3f508c6b601750eac0237e
David Mghanga <damghanga@gmail.com>
2024-05-29 10:57:25 +0300

    Updated code formatting to pass linting.

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,13 +2,16 @@
 Calculator functions
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
     return x + y 
 
+
 def subtract(x, y):
     """Subract x from y and return result."""
     return 0
+
 
 def subtract(x, y):
     """subtract x from y and return result."""
